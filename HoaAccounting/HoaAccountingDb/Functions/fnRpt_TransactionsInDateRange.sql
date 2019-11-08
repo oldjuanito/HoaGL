2874f4c31443ec852fb6435fdb3e995fdee2cb13
Juan Tarquino <jptarqu@gmail.com>
2019-11-07 19:31:26 -0500

    Estoppel in report

--- HoaAccounting/HoaAccountingDb/Functions/fnRpt_TransactionsInDateRange.sql
+++ HoaAccounting/HoaAccountingDb/Functions/fnRpt_TransactionsInDateRange.sql
@@ -10,12 +10,14 @@ RETURNS TABLE AS RETURN
 	SELECT 
 		--SUM(t.Amount * t.CreditDebitFlag) OVER (ORDER BY t.[GLTransactionId]) RunningTotal,
 		t.TransactionDate,
-		t.ReportDescription,
+		case when es.HouseNumber is null then t.ReportDescription else 'Estoppel for ' + cast(es.HouseNumber as varchar(4)) end ReportDescription,
 		t.CheckNumber,
 		t.Amount
 	from GLTransactions t
 	inner join GLAccount a
 	on t.AccountId = a.AccountId
+	left join Estoppels es
+	on t.GLTransactionId = es.GLTransactionId
 	where t.TransactionDate between @StartDate and @EndDate
 	and t.AccountId = @AccountId
 	and t.CreditDebitFlag = @CreditDebitFlag
